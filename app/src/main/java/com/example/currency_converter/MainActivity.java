7fadbf0dfd9b565fff51d0cace735400975f07d9
john jorjill <orgil9506@gmail.com>
2020-05-05 00:15:35 +0800

    finished app

--- app/src/main/java/com/example/currency_converter/MainActivity.java
+++ app/src/main/java/com/example/currency_converter/MainActivity.java
@@ -3,8 +3,23 @@ package com.example.currency_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view){
+
+        EditText userInput = findViewById(R.id.editText2);
+
+        Double mnt = Double.parseDouble(userInput.getText().toString());
+        Double usd = mnt/ 2800;
+
+        Toast.makeText(this, usd.toString().substring(0, 5), Toast.LENGTH_SHORT).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
